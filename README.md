18dd1a7ba734a7cbca882351b690d02f6a8a6ec4
Filip Krunic <fkrunic9@gmail.com>
2023-09-03 21:46:11 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Social Proof Section
+Implementation of [Social Proof Section](https://www.frontendmentor.io/challenges/social-proof-section-6e0qTv_bA) using Vue and TailwindCSS.
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.body.className = ''
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+

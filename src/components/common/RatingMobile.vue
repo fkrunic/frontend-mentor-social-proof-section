63f73eb45c685baa2555bb92adaf79ef803b6b76
Filip Krunic <fkrunic9@gmail.com>
2023-09-03 22:15:37 -0700

    basic component logic for mobile

--- src/components/common/RatingMobile.vue
+++ src/components/common/RatingMobile.vue
@@ -1,3 +1,12 @@
 <script setup lang="ts">
 defineProps<{ ratingSpot: string }>()
 </script>
+
+<template>
+    <div class="flex flex-col gap-2">
+        <div class="flex flex-row gap-2">
+            <img v-for="_ of [1,2,3,4,5]" src="../../assets/icon-star.svg" alt="star">
+        </div>
+        <p>Rated 5 Stars in {{ ratingSpot }}</p>
+    </div>
+</template>
--- src/components/common/ReviewMobile.vue
+++ src/components/common/ReviewMobile.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
 import { Review } from '../../data'
-defineProps<{ review: Review }>()
+defineProps<{ review: Review, portraitTag: string }>()
 </script>
+
+<template>
+    <div class="flex flex-col p-4 items-center">
+        <div class="flex flex-row gap-2">
+            <img :src="`../../assets/image-${review.portraitTag}.jpg`" alt="portrait">
+            <div class="flex flex-col">
+                <p class="text-left">{{ review.name }}</p>
+                <p class="text-left">Verified Buyer</p>
+            </div>
+        </div>
+        <p class="text-left">" {{ review.review }} "</p>
+    </div>
+</template>
